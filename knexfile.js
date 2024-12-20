319ede4e3608c239f0e433aa4a72c8142b810276
Gabriel Vitor <125525068+zvitorll@users.noreply.github.com>
2024-12-20 10:13:07 -0300

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: 'mysql2',
+    connection: {
+      host : '127.0.0.1',
+      user : 'seu_usuario',
+      password : 'sua_senha',
+      database : 'pettopstore'
+    }
+   };
---
+++ migrations/20241220124542_create_employees.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('clients', function (table) {
+      table.increments('id');
+      table.string('email');
+      table.string('password');
+      table.string('name');
+    })
+   };
+   
+   exports.down = function(knex) {
+    return knex.schema.dropTable('clients');
+   };
